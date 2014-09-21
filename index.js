0e13d87f05fcd6fe930c11cdfd3573e492968229
Martin Abente Lahaye <tch@sugarlabs.org>
2014-09-21 00:43:51 -0400

    Support movements and chat
    Signed-off-by: Martin Abente Lahaye <tch@sugarlabs.org>

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
-var express = require('express')
+var express = require("express")
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
-app.get('/', function(req, res){
-  res.sendFile('/home/tch/Devel/javascript/test/server/index.html');
+app.get("/", function(req, res){
+  res.sendFile("/home/tch/Devel/javascript/test/server/index.html");
 });
 
 var buddies = {}
 
-io.on('connection', function(socket){
-  console.log('user connected');
+io.on("connection", function(socket){
+  console.log("user connected");
 
   var buddy = {}
-  buddy['name'] = 'Unnamed';
+  buddy["id"] = socket.id;
+  buddy["name"] = "Unnamed";
   buddy["x"] = Math.floor((Math.random() * (256 - 40)) + 20);
   buddy["y"] = Math.floor((Math.random() * (256 - 40)) + 20);
   buddies[socket.id] = buddy;
- 
-  io.emit('update', buddies);
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.emit("initialized", socket.id, buddies); 
+  socket.broadcast.emit("added", buddy);
+
+  socket.on("update", function(properties){
+    console.log("user updated");
+
+    if ("x" in properties) {
+      buddy["x"] = properties["x"];
+    }
+    if ("y" in properties) {
+      buddy["y"] = properties["y"];
+    }
+    if ("name" in properties) {
+      buddy["name"] = properties["name"];
+    }
+
+    socket.broadcast.emit("updated", buddy);
+  });
+
+  socket.on("disconnect", function(){
+    console.log("user disconnected");
     delete buddies[socket.id];
-    io.emit('update', buddies);
+    socket.broadcast.emit("removed", socket.id);
   });
 
 });
