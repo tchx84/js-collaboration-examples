21ca9bb60ba5aad0cfb2b8b52782feac91ee5580
Martin Abente Lahaye <tch@sugarlabs.org>
2014-09-20 23:00:55 -0400

    Basic lobby implementation
    Signed-off-by: Martin Abente Lahaye <tch@sugarlabs.org>

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
-var app = require('express')();
+var express = require('express')
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile('/home/tch/Devel/javascript/test/server/static/index.html');
+  res.sendFile('/home/tch/Devel/javascript/test/server/index.html');
 });
 
-var connections = 0;
+var buddies = {}
 
 io.on('connection', function(socket){
   console.log('user connected');
-  connections += 1;
-  io.emit('update', connections);
+
+  var buddy = {}
+  buddy['name'] = 'Unnamed';
+  buddy["x"] = Math.floor((Math.random() * (256 - 40)) + 20);
+  buddy["y"] = Math.floor((Math.random() * (256 - 40)) + 20);
+  buddies[socket.id] = buddy;
+ 
+  io.emit('update', buddies);
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
-    connections -= 1;
-    io.emit('update', connections);
+    delete buddies[socket.id];
+    io.emit('update', buddies);
   });
+
 });
 
 http.listen(3000, function(){
